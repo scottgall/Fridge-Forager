aa8bbd91d09989cc09c81dbad4e95208881f8b7e
joannelee1990 <33495443+joannelee1990@users.noreply.github.com>
2018-05-03 18:13:53 -0500

    first push

---
+++ javascript/app.js
@@ -0,0 +1,13 @@
+  $(document).ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left',
+      hoverEnabled: false
+    });
+  });
+        
+        
